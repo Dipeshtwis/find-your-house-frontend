8918a11f28921832b639d2ce54d8c5d97f0cd833
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-24 17:34:57 +0530

    Pass the loggedinstatus to home component

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,27 @@ import Home from '../containers/Home';
 import Dashboard from '../containers/Dashboard';
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {}
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <BrowserRouter>
           <Switch>
-            <Route exact path={"/"} component={Home} />
+            <Route
+              exact 
+              path={"/"}
+              render={props => (
+                <Home {...props} loggedInStatus={this.state.loggedInStatus} />
+              )}
+            />
             <Route exact path={"/dashboard"} component={Dashboard} />
           </Switch>
         </BrowserRouter>
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import Registration from '../components/Registration';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
   render() {
     return (
       <>
         <h1>Home</h1>
+        <h2>Status: {this.props.loggedInStatus}</h2>
         <Registration />
       </>
     );
