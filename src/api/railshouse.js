623642402c4684501e167b80aecc1278006ff141
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-25 21:01:23 +0530

    Fetch the houses in console via api

--- src/api/railshouse.js
+++ src/api/railshouse.js
@@ -3,3 +3,4 @@ export const API_LOGIN = 'sessions';
 export const API_REGISTRATION = 'registrations';
 export const API_LOGOUT = 'logout';
 export const API_LOGIN_STATUS = 'logged_in';
+export const API_HOUSE = 'houses';
--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { API_ID, API_LOGOUT } from '../api/railshouse';
+import { API_ID, API_LOGOUT, API_HOUSE } from '../api/railshouse';
 
 const Dashboard = props => {
   const { loggedInStatus, handleLogout } = props;
-  
+  const [houses, SetHouses] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${API_ID}${API_HOUSE}`)
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log("house fetching error", err);
+    });
+  }, [houses.length])
+
   const handleLogoutClick = () => {
   	axios.delete(`${API_ID}${API_LOGOUT}`, {
   	  withCredentials: true
@@ -20,11 +31,13 @@ const Dashboard = props => {
   }
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <button onClick={() => handleLogoutClick()}>Logout</button>
-      <h2>Satatus: {loggedInStatus}</h2>
-    </div>
+    <>
+      <div>
+        <h1>Dashboard</h1>
+        <button onClick={() => handleLogoutClick()}>Logout</button>
+        <h2>Status: {loggedInStatus}</h2>
+      </div>
+    </>
   );
 };
 
