3a15dfc3b685556902b88d11876c04c0f19e7953
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-01-25 19:00:03 +0530

    Create store and include provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './reducers/index';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
-import { combineReducers } from 'redux';
+import { combineReducers, createStore } from 'redux';
 import houseReducer from './house';
 
 const rootReducer = combineReducers({
   house: houseReducer,
 });
+
+const store = createStore(rootReducer);
+
+export default store;
